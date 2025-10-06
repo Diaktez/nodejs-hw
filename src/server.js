1a6b484e033b3489f6a8ce38d6847e24ca3f6205
Aleksei <bylexatime@gmail.com>
2025-10-06 19:24:16 +0300

    test 1

--- src/server.js
+++ src/server.js
@@ -1,67 +1,41 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import pino from 'pino-http';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-// Використовуємо значення з .env або дефолтний порт 3000
 const PORT = process.env.PORT ?? 3030;
 
-app.use(express.json());
-app.use(cors());
+app.use(logger);
 app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
+  express.json({
+    limit: '100kb',
   }),
 );
+app.use(cors());
 
-// Перший маршрут
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello node' });
+  res.status(200).json({ message: 'Hello user' });
 });
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(notesRoutes);
 
-// Список усіх notes
-app.get('/notes', (req, res) => {
-  res.status(200).json([{ message: 'Retrieved all notes' }]);
-});
-
-// Конкретна Note за id
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-app.get('/test-error', () => {
+app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
 });
 
-// Middleware для неіснуючих маршрутів
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(notFoundHandler);
 
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: 'Simulated server error',
-    error: err.message,
-  });
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
